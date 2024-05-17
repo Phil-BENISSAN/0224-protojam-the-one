09576d2861c38c519b85b962a9e3c09f955e1e28
pjroggy <pierrejeanroggy@gmail.com>
2024-05-17 11:58:06 +0200

    add analyse page

--- client/src/pages/AnalysePage.jsx
+++ client/src/pages/AnalysePage.jsx
@@ -1,8 +1,37 @@
+import "./AnalysePage.css";
 
 function AnalysePage() {
-  return (
-    <div>AnalysePage</div>
-  )
+  return (<>
+    <h2 className="analyseTitle">Analyse</h2>
+    <div className="analyseImageContainer">
+      <img
+        className="tempsEcran"
+        src="/src/assets/temps_écrans.PNG"
+        alt="Temps écran"
+      />
+      <img
+        className="tempsEcran"
+        src="/src/assets/temps_depense_sans_pratique.png"
+        alt="Temps dépensé sans pratique"
+      />
+      <img
+        className="tempsEcran"
+        src="/src/assets/temps_depense_avec_pratique.png"
+        alt="Temps dépensé avec pratique"
+      />
+      <img
+        className="tempsEcran"
+        src="/src/assets/temps_entrainement_sans.png"
+        alt="Temps entrainement sans pratique"
+      />
+      <img
+        className="tempsEcran"
+        src="/src/assets/temps_entrainement_avec.png"
+        alt="Temps entrainement avec pratique"
+      />
+    </div>
+    </>
+  );
 }
 
-export default AnalysePage
+export default AnalysePage;
