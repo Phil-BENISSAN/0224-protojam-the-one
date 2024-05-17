e15bf03bf509698db07597d3cce8f33ad8e96fd3
Belin <sorrento974@gmail.com>
2024-05-17 12:18:03 +0200

    fix css +others

--- client/src/components/DailyCard.jsx
+++ client/src/components/DailyCard.jsx
@@ -2,24 +2,19 @@ import PropTypes from "prop-types";
 import "../components/DailyCard.css";
 import MapComponent from "./MapComponents";
 
-// import {
-//   MapContainer,
-//   TileLayer,
-//   Marker,Popup
-// } from 'https://cdn.esm.sh/react-leaflet'
-
 function DailyCard({ data }) {
   return (
     <div className="card">
       <div className="card__content"></div>
       <article className="positionCard">
-        <h2 className="positionTitle">{data.nom}</h2>
+        <h2 className="Title">{data.nom}</h2>
         <img className="positionImg" src={data.image} alt="a yoga position" />
         <div className="paragraphe">
-          <h2>Description</h2>
+          <h2 className="Title">Description</h2>
           <p className="description">{data.description}</p>
-          <h2>Instructions : </h2>
+          <h2 className="Title">Instructions</h2>
           <p className="instructions">{data.instructions}</p>
+          <h2 className="Title">Spots à proximité</h2>
           <MapComponent />
         </div>
       </article>
